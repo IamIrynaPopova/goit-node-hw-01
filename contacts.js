254612266dd087a181a457b160af9d292bf1c179
Iryna Popova <popovaira969@gmail.com>
2023-05-21 21:30:32 +0300

    ADDED:export and import  module CommonJS

--- contacts.js
+++ contacts.js
@@ -15,48 +15,55 @@ async function listContacts() {
   }
 }
 
-// async function getContactById(contactId) {
-//   try {
-//     const data = await listContacts();
-//     const contact = await data.find((contact) => contact.id === contactId);
-//     return console.table(contact);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+async function getContactById(contactId) {
+  try {
+    const data = await listContacts();
+    const contact = await data.find((contact) => contact.id === contactId);
+    return console.table(contact);
+  } catch (err) {
+    console.error(err);
+  }
+}
 // getContactById("drsAJ4SHPYqZeG-83QTVW");
 
-// async function addContact(name, email, phone) {
-//   try {
-//     const data = await listContacts();
-//     const updateContacts = await data.push({
-//       id: uuidv4(),
-//       name,
-//       email,
-//       phone,
-//     });
-//     await fs.writeFile(contactsPath, JSON.stringify(data));
-//     console.log(`Contact "${name}" was added to the phonebook`);
-//     return data;
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+async function addContact(name, email, phone) {
+  try {
+    const data = await listContacts();
+    const updateContacts = await data.push({
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    });
+    await fs.writeFile(contactsPath, JSON.stringify(data));
+    console.log(`Contact "${name}" was added to the phonebook`);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 // addContact("Iryna Popova", "irina@gmail.com", "(748) 202-5555");
 
-// async function removeContact(contactId) {
-//   try {
-//     const data = await listContacts();
-//     const filterContacts = await data.filter(
-//       (contact) => contact.id !== contactId
-//     );
-//     await fs.writeFile(contactsPath, JSON.stringify(filterContacts));
-//     console.log(`Contact with id "${contactId}" was deleted`);
-//     return filterContacts;
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+async function removeContact(contactId) {
+  try {
+    const data = await listContacts();
+    const filterContacts = await data.filter(
+      (contact) => contact.id !== contactId
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(filterContacts));
+    console.log(`Contact with id "${contactId}" was deleted`);
+    return filterContacts;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 // removeContact("drsAJ4SHPYqZeG-83QTVW");
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log('Hello world');
-console.log('Test');
+const contacts = require('./contacts');
+
+// contacts.listContacts();
+// contacts.getContactById("drsAJ4SHPYqZeG-83QTVW");
+// contacts.addContact("Iryna Popova", "irina@gmail.com", "(748) 202-5555");
+// contacts.removeContact("22885d8d-4bbd-44fc-90be-cee76ffa4cba");
